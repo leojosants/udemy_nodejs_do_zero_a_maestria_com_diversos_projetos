9cf85dbb0aef65ac3074f32c0a91fb0809f4182a
Leonardo Santos <leonardojosantos@gmail.com>
2023-02-06 19:29:34 -0300

    Criação de conta.

--- SECAO_5-PROJETO_1-ACCOUNTS/index.js
+++ SECAO_5-PROJETO_1-ACCOUNTS/index.js
@@ -18,7 +18,7 @@ function operation() {
                 message: 'O que você deseja fazer?',
                 choices:
                     [
-                        'Criar conta',
+                        'Criar Conta',
                         'Consultar saldo',
                         'Depositar',
                         'Sacar',
@@ -29,8 +29,18 @@ function operation() {
     )
         .then((answer) => {
             const action = answer['action']
-            console.log(action);
+            
+            if (action === 'Criar Conta') {
+                createAccount();
+            }
+
         })
         .catch((err) => console.log(err))
 
+};
+
+// create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir:'));
 };
