4495ff4e8891bbcfc7fd7c65dd0b37beb5655ca9
Leonardo Santos <leonardojosantos@gmail.com>
2023-02-06 18:46:28 -0300

    Setup inicial.

---
+++ SECAO_5-PROJETO_1-ACCOUNTS/index.js
@@ -0,0 +1,9 @@
+
+// Modulos externos
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+// Modulos internos
+const fs = require('fs');
+
+console.log('Iniciamos o Accouts!');
