af610a8c56f2358d58693316f32f4b78323632b2
Leonardo Santos <leonardojosantos@gmail.com>
2023-02-05 22:18:38 -0300

    Retornando HTML.

---
+++ SECAO_3-NODE_CORE_MODULES/2-retornando_html/index.js
@@ -0,0 +1,16 @@
+// 
+const http = require('http');
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+
+    // Habilitando envio de html
+    res.setHeader('contenty-Type', 'text/html');
+
+    res.end('<h1>Olá, este é meu primeiro server com HTML.</h1>');
+});
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`); 
+});
