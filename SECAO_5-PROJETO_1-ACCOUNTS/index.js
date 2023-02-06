06174ff7cef9735699e15f3405c871b13736da95
Leonardo Santos <leonardojosantos@gmail.com>
2023-02-06 19:00:12 -0300

    Iniciaização do sistema

--- SECAO_5-PROJETO_1-ACCOUNTS/index.js
+++ SECAO_5-PROJETO_1-ACCOUNTS/index.js
@@ -6,4 +6,31 @@ const chalk = require('chalk');
 // Modulos internos
 const fs = require('fs');
 
-console.log('Iniciamos o Accouts!');
+operation();
+
+function operation() {
+
+    inquirer.prompt(
+        [
+            {
+                type: 'list',
+                name: 'action',
+                message: 'O que você deseja fazer?',
+                choices:
+                    [
+                        'Criar conta',
+                        'Consultar saldo',
+                        'Depositar',
+                        'Sacar',
+                        'Sair'
+                    ]
+            },      
+        ]
+    )
+        .then((answer) => {
+            const action = answer['action']
+            console.log(action);
+        })
+        .catch((err) => console.log(err))
+
+};
