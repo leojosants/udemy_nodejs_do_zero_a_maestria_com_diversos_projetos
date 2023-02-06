b07fb632de0c87801a1e7281773651e55cd43333
Leonardo Santos <leonardojosantos@gmail.com>
2023-02-06 20:36:21 -0300

    Finalizando criação de conta.

--- SECAO_5-PROJETO_1-ACCOUNTS/index.js
+++ SECAO_5-PROJETO_1-ACCOUNTS/index.js
@@ -10,23 +10,24 @@ operation();
 
 function operation() {
 
-    inquirer.prompt(
-        [
-            {
-                type: 'list',
-                name: 'action',
-                message: 'O que você deseja fazer?',
-                choices:
-                    [
-                        'Criar Conta',
-                        'Consultar saldo',
-                        'Depositar',
-                        'Sacar',
-                        'Sair'
-                    ]
-            },      
-        ]
-    )
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: 'O que você deseja fazer?',
+                    choices:
+                        [
+                            'Criar Conta',
+                            'Consultar Saldo',
+                            'Depositar',
+                            'Sacar',
+                            'Sair'
+                        ],
+                },      
+            ]
+        )
         .then((answer) => {
             const action = answer['action']
             
@@ -43,4 +44,51 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
     console.log(chalk.green('Defina as opções da sua conta a seguir:'));
+
+    buildAccount();
+
+};
+
+function buildAccount() {
+    
+    inquirer
+        .prompt(
+            [
+                {
+                    name: 'accountName',
+                    messagem: 'Digite um nome para a sua conta:'
+                }
+            ]
+        )
+        .then(answer => {
+            const accountName = answer['accountName'];
+            
+            console.info(accountName);
+
+            if (!fs.existsSync('accounts')) {
+                fs.mkdirSync('accounts');
+            }
+
+            if (fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'),);
+
+                buildAccount();
+
+                return;
+            }
+
+            fs.writeFileSync(
+                `accounts/${accountName}.json`,
+                '{"balance": 0}',
+                function (err) {
+                    console.log(err);
+                },
+            );
+
+            console.log(chalk.green('Parabéns, a sua Conta foi criada!'));
+
+            operation();
+
+        })
+        .catch((err => console.log(err)))
 };
