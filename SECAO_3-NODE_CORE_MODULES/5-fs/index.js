3524ff045c63aae045ad8485e03fdd52f1ffe61f
Leonardo Santos <leonardojosantos@gmail.com>
2023-02-05 23:12:32 -0300

    Renderizando HTML com o fs.

---
+++ SECAO_3-NODE_CORE_MODULES/5-fs/index.js
@@ -0,0 +1,19 @@
+// 
+const http = require('http');
+const fs = require('fs');
+const port = 3001;
+
+// Criando servidor HTTP
+const server = http.createServer((req, res) => {
+
+    fs.readFile('mensagem.html', function (err, data) {
+        res.writeHead(200, { 'content-Type': 'text/html' })
+        res.write(data);
+        return res.end();
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`); 
+});
