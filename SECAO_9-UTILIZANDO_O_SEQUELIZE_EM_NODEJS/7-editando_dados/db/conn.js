7c5050e625f53c3af05a4674dbf17a75d7d38c40
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-03 00:29:30 -0300

    Editando dados.

---
+++ SECAO_9-UTILIZANDO_O_SEQUELIZE_EM_NODEJS/7-editando_dados/db/conn.js
@@ -0,0 +1,19 @@
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodesequelize', 'root', '',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+);
+
+// try {
+//     sequelize.authenticate();
+//     console.log('Conectamos com sucesso com o Sequelize!');
+// }
+// catch (error) {
+//     console.log('Não foi possível conectar: ', error);
+// }
+
+module.exports = sequelize;
