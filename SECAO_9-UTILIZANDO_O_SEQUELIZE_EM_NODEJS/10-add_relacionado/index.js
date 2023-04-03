66f9d28743318cc4df75d13cfe27d5d78c2924b1
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-03 12:44:23 -0300

    Adionando dado relacionado.

---
+++ SECAO_9-UTILIZANDO_O_SEQUELIZE_EM_NODEJS/10-add_relacionado/index.js
@@ -0,0 +1,119 @@
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/conn');
+
+const User = require('./models/User');
+const Address = require('./models/Address');
+
+const app = express();
+
+// CONFIGURANDO O BODY
+app.use(express.urlencoded({ extended: true }));
+
+// CAPTURANDO O BODY EM JSON    
+app.use(express.json());
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.get('/users/create', (req, res) => { res.render('addUsers'); });
+
+// 
+app.post('/users/create', async (req, res) => {
+
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+
+    let newsletter = req.body.newsletter;
+
+    if (newsletter === 'on') { newsletter = true; }
+    else { newsletter = false; }
+
+    console.log(req.body);
+
+    await User.create({ name, occupation, newsletter });
+
+    res.redirect('/');
+
+});
+
+// 
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findOne({ raw: true, where: { id: id } });
+    res.render('userView', { user });
+});
+
+// 
+app.post('/users/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    await User.destroy({ where: { id: id } });
+    res.redirect('/');
+});
+
+// 
+app.get('/users/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findOne({ raw: true, where: { id: id } });
+    res.render('userEdit', { user });
+});
+
+// 
+app.post('/users/update', async (req, res) => {
+
+    const id = req.body.id;
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+
+    let newsletter = req.body.newsletter;
+
+    if (newsletter === 'on') { newsletter = true; }
+    else { newsletter = false; }
+
+    const userData = {
+        id: id,
+        name: name,
+        occupation: occupation,
+        newsletter: newsletter
+    }
+
+    await User.update(userData, { where: { id: id } });
+
+    res.redirect('/');
+
+});
+
+// 
+app.get('/', async (req, res) => {
+    const users = await User.findAll({ raw: true });
+    console.log(users);
+    res.render('home', { users: users });
+});
+
+// 
+app.post('/address/create', async (req, res) => {
+
+    const UserId = req.body.UserId;
+    const street = req.body.street;
+    const number = req.body.number;
+    const city = req.body.city;
+
+    const address = {
+        UserId,
+        street,
+        number,
+        city
+    };
+
+    await Address.create(address);
+
+    res.redirect(`/users/edit/${UserId}`);
+
+});
+
+// 
+// conn.sync({ force: true }) // PARA RESET DA TABELA
+conn.sync()
+    .then(() => { app.listen(3000); })
+    .catch((error) => console.log(error));
---
+++ SECAO_9-UTILIZANDO_O_SEQUELIZE_EM_NODEJS/10-add_relacionado/models/Address.js
@@ -0,0 +1,32 @@
+
+const { DataTypes } = require('sequelize');
+const db = require('../db/conn');
+const User = require('./User');
+
+// CRIANDO MODEL / SCHEMA
+const Address = db.define('Address',
+    {
+        street:
+        {
+            type: DataTypes.STRING,
+            require: true
+        },
+
+        number:
+        {
+            type: DataTypes.STRING,
+            require: true
+        },
+
+        city:
+        {
+            type: DataTypes.STRING,
+            require: true
+        },
+    }
+);
+
+// RELAÇÃO
+Address.belongsTo(User);
+
+module.exports = Address;
