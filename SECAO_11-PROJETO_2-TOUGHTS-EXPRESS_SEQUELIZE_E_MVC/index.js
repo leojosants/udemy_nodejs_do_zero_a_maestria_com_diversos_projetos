3d09420d269b7fdd4880f361d46c6fa24a807be1
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-04 19:46:37 -0300

    Finalizando estrutura do projeto.

--- SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/index.js
+++ SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/index.js
@@ -6,14 +6,50 @@ const FileStore = require('session-file-store')(session);
 const flas = require('express-flash');
 const app = express();
 const conn = require('./db/conn');
+const flash = require('express-flash');
 
-// app.engine('handlebars', exphbs.engine());
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
-// app.use(express.urlencoded({ extended: true }));    //  CONFIGURANDO O BODY
-// app.use(express.json());                            //  CAPTURANDO O BODY EM JSON
-// app.use(express.static('public'));                  //  ACESSANDO PASTA ESTÁTICA
+app.use(express.urlencoded({ extended: true }));    //  CONFIGURANDO O BODY
+app.use(express.json());                            //  CAPTURANDO O BODY EM JSON
 
+// SESSION MIDDLEWARE
+app.use(
+    session(
+        {
+            name: 'session',
+            secret: 'nosso_secret',
+            resave: false,
+            saveUninitialized: false,
+
+            store: new FileStore(
+                {
+                    logFn: function () { },
+                    path: require('path').join(require('os').tmpdir(), 'session')
+                }
+            ),
+
+            cookie: {
+                secure: false,
+                maxAge: 360000,
+                expires: new Date(Date.now() + 360000),
+                httpOnly: true
+            }
+        }
+    )
+);
+
+app.use(flash());                   //  FLASH MESSAGES
+app.use(express.static('public'));  //  ACESSANDO PASTA ESTÁTICA
+
+// SET SESSION TO RES
+app.use((req, res, next) => {
+    if (req.session.userid) { res.locals.session = req.session }
+    next();
+});
+
+// CONEXÃO COM BD
 conn.sync()
     .then(() => { app.listen(3000) })
     .catch((err) => console.log(err));
