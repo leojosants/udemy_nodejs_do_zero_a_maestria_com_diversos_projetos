a3e2261f76ed0a180ab8d330ff407332e74f2715
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-04 19:21:21 -0300

    Iniciando estrutura do projeto.

--- SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/index.js
+++ SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/index.js
@@ -1,1 +1,19 @@
 
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flas = require('express-flash');
+const app = express();
+const conn = require('./db/conn');
+
+// app.engine('handlebars', exphbs.engine());
+// app.set('view engine', 'handlebars');
+
+// app.use(express.urlencoded({ extended: true }));    //  CONFIGURANDO O BODY
+// app.use(express.json());                            //  CAPTURANDO O BODY EM JSON
+// app.use(express.static('public'));                  //  ACESSANDO PASTA ESTÁTICA
+
+conn.sync()
+    .then(() => { app.listen(3000) })
+    .catch((err) => console.log(err));
