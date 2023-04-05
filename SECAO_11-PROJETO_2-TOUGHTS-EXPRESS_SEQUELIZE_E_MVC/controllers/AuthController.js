39b22a93768360c2a4844fd48b906d95a9a64e89
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-05 00:26:48 -0300

    Registrando usuario no sistema.

--- SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/controllers/AuthController.js
+++ SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/controllers/AuthController.js
@@ -11,11 +11,39 @@ module.exports = class AuthController {
 
         const { name, email, password, confirmPassword } = req.body;
 
-        // PASSWORD MATCH VALIDATION
+        //  PASSWORD MATCH VALIDATION
         if (password != confirmPassword) {
             req.flash('message', 'As senhas não conferem, tente novamente!');
             res.render('auth/register');
             return;
         }
+
+        //  CHECK IF USER EXISTS
+        const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+        if (checkIfUserExists) {
+            req.flash('message', 'O email já está em uso!');
+            res.render('auth/register');
+            return;
+        }
+
+        //  CREATE A PASSWORD
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        const user = { name, email, password: hashedPassword };
+
+        try {
+            const createdUser = await User.create(user);
+
+            //  INITIALIZE SESSION
+            req.session.userid = createdUser.id;
+
+            req.flash('message', 'Cadastro realizado com sucesso!');
+
+            req.session.save(() => { res.redirect('/'); });
+
+        }
+        catch (error) { console.log(error); }
+
     };
 };
