8bfe13d6062a1195306334fd3607a06a90bbee07
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-06 17:42:33 -0300

    Criando funcionalidade de busca.

--- SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/controllers/ToughtController.js
+++ SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/controllers/ToughtController.js
@@ -1,16 +1,35 @@
 
 const Tought = require('../models/Tought');
 const User = require('../models/User');
+const { Op } = require('sequelize');
 
-//  EXPORTANDO MÉTODOS STÁTICOS
+//  EXPORTANDO MÉTODOS ESTÁTICOS
 module.exports = class ToughtController {
 
     static async showToughts(req, res) {
 
-        const toughtsData = await Tought.findAll({ include: User });
+        let search = '';
+
+        if (req.query.search) { search = req.query.search; }
+
+        const toughtsData = await Tought.findAll(
+            {
+                include: User,
+
+                where:
+                {
+                    title: { [Op.like]: `%${search}%` }
+                }
+            }
+        );
+
         const toughts = toughtsData.map((result) => result.get({ plain: true }));
 
-        res.render('toughts/home', { toughts });
+        let toughtsQty = toughts.length;
+
+        if (toughtsQty === 0) { toughtsQty = false; }
+
+        res.render('toughts/home', { toughts, search, toughtsQty });
     };
 
     static async dashboard(req, res) {
