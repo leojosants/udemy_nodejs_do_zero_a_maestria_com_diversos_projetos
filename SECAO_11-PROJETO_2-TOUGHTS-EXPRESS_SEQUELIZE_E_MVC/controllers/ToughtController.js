657a88453977dc42e374190e9d62bc07458ff9a5
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-06 14:46:47 -0300

    Mensagem para dashboard vazia.

--- SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/controllers/ToughtController.js
+++ SECAO_11-PROJETO_2-TOUGHTS-EXPRESS_SEQUELIZE_E_MVC/controllers/ToughtController.js
@@ -24,7 +24,11 @@ module.exports = class ToughtController {
 
         const toughts = user.Toughts.map((result) => result.dataValues);
 
-        res.render('toughts/dashboard', { toughts });
+        let emptyToughts = false;
+
+        if (toughts.length === 0) { emptyToughts = true; }
+
+        res.render('toughts/dashboard', { toughts, emptyToughts });
     };
 
     static createTought(req, res) { res.render('toughts/create'); };
