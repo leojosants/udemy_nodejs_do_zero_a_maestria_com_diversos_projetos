7913702a5a1b448bf10cbceace8eda3687b926ff
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-12 19:52:08 -0300

    Função de login no sistema.

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/controllers/UserController.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/controllers/UserController.js
@@ -59,4 +59,38 @@ module.exports = class UserController {
         }
         catch (error) { res.status(500).json({ message: error }); }
     };
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório!' });
+            return;
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória!' });
+            return;
+        }
+
+        //  Check if exists
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            res.status(422).json({ message: 'Não há usuário cadastrado com este e-mail!' });
+            return;
+        }
+
+        //  Check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if (!checkPassword) {
+            res.status(422).json({ message: 'Senha inválida!' });
+            return;
+        }
+
+        await createUserToken(user, req, res);
+    };
+
+
 };
