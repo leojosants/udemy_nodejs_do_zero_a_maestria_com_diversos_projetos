3fc0d9399dc9a986ad541cb12f804b4ec0707946
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-13 13:18:55 -0300

    Verificação do token do usuário.

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/routes/UserRoutes.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/routes/UserRoutes.js
@@ -1,11 +1,17 @@
 // 
 const router = require('express').Router();
 
+//  Controlers  //
 const UserController = require('../controllers/UserController');
 
+//  Middlewares //
+const verifyToken = require('../helpers/verify-token');
+
+//  Routes  //
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/checkUser', UserController.checkUser);
 router.get('/:id', UserController.getUserById);
+router.patch('/edit/:id', verifyToken, UserController.editUser);
 
 module.exports = router;
