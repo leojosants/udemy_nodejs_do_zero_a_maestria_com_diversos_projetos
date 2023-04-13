a7ba5908f4d13decf3a6ccd1bc8109dac06a1c02
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-13 16:51:54 -0300

    Salvando Pet no sistema.

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/controllers/PetController.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/controllers/PetController.js
@@ -1,11 +1,48 @@
 // 
 const Pet = require('../models/Pet');
 
-module.exports = class PetController { 
+//  Helpers
+const getToken = require('../helpers/get-token');
+const getUserByToken = require('../helpers/get-user-by-token');
 
+module.exports = class PetController {
 
     //  Create a Pet
     static async create(req, res) {
-        res.json({message: 'Pet criado com sucesso!'});
-     };
+        const { name, age, weight, color } = req.body;
+        const available = true;
+
+        //  Images upload
+
+        //  Validation
+        if (!name) { res.status(422).json({ message: 'O nome é obrigatório!' }); return; }
+        if (!age) { res.status(422).json({ message: 'A idade é obrigatória!' }); return; }
+        if (!weight) { res.status(422).json({ message: 'O peso é obrigatório!' }); return; }
+        if (!color) { res.status(422).json({ message: 'O cor é obrigatória!' }); return; }
+
+        //  Get pet owner
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        //  Create a pet
+        const pet = new Pet(
+            {
+                name, age, weight, color, available,
+                images: [],
+
+                user: {
+                    _id: user.id,
+                    name: user.name,
+                    image: user.image,
+                    phone: user.phone
+                }
+            }
+        );
+
+        try {
+            const newPet = await pet.save();
+            res.status(201).json({ message: 'Pet cadastrado com sucesso!', newPet });
+        } catch (error) { res.status(500).json({ message: error }); }
+
+    };
 };
--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/routes/PetRoutes.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/routes/PetRoutes.js
@@ -4,6 +4,9 @@ const router = require('express').Router();
 //  Controller
 const PetController = require('../controllers/PetController');
 
-router.post('/create', PetController.create);
+//  Middlewares
+const verifyToken = require('../helpers/verify-token');
+
+router.post('/create',verifyToken, PetController.create);
 
 module.exports = router;
