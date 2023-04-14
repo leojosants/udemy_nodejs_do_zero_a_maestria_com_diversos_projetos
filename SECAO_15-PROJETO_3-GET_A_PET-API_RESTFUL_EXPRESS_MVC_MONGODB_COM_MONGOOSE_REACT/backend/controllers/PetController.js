81dd2e3fbb55a9456af6cfdd3c45f9591b5e039f
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-13 22:48:23 -0300

    Atualização do Pet.

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/controllers/PetController.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/controllers/PetController.js
@@ -98,7 +98,7 @@ module.exports = class PetController {
 
         //  Check if pet exists
         const pet = await Pet.findOne({ _id: id });
-        if (!pet) { res.status(404).json({ message: 'Pet não encontrado!' }); }
+        if (!pet) { res.status(404).json({ message: 'Pet não encontrado!' }); return; }
 
         //  Check if logged in user registered the pet
         const token = getToken(req);
@@ -108,7 +108,52 @@ module.exports = class PetController {
             res.status(422).json({
                 message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'
             });
+            return;
         }
 
+        await Pet.findByIdAndRemove(id);
+        res.status(200).json({ message: 'Pet removido com sucesso!' });
+    };
+
+    static async updatePet(req, res) {
+        const id = req.params.id;
+        const { name, age, weight, color, available } = req.body;
+        const images = req.files;
+        const updatedData = {};
+
+        //  Check if pet exists
+        const pet = await Pet.findOne({ _id: id });
+        if (!pet) { res.status(404).json({ message: 'Pet não encontrado!' }); return; }
+
+        //  Check if logged in user registered the pet
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        if (pet.user._id.toString() !== user._id.toString()) {
+            res.status(422).json({
+                message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'
+            });
+            return;
+        }
+
+        //  Validation
+        if (!name) { res.status(422).json({ message: 'O nome é obrigatório!' }); return; }
+        else { updatedData.name = name; }
+
+        if (!age) { res.status(422).json({ message: 'A idade é obrigatória!' }); return; }
+        else { updatedData.age = age; }
+
+        if (!weight) { res.status(422).json({ message: 'O peso é obrigatório!' }); return; }
+        else { updatedData.weight = weight; }
+
+        if (!color) { res.status(422).json({ message: 'O cor é obrigatória!' }); return; }
+        else { updatedData.color = color; }
+
+        if (images.length === 0) { res.status(422).json({ message: 'O imagem é obrigatória!' }); return; }
+        else { updatedData.images = []; }
+        images.map((image) => { updatedData.images.push(images.filename) });
+
+        await Pet.findByIdAndUpdate(id, updatedData);
+        res.status(200).json({ message: 'Pet atualizado com sucesso!' });
     };
 };
--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/routes/PetRoutes.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/backend/routes/PetRoutes.js
@@ -14,5 +14,6 @@ router.get('/myPets', verifyToken, PetController.getAllUserPets);
 router.get('/myAdoptions', verifyToken, PetController.getAllUserAdoptions);
 router.get('/:id', PetController.getPetById);
 router.delete('/:id', verifyToken, PetController.removePetById);
+router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet);
 
 module.exports = router;
