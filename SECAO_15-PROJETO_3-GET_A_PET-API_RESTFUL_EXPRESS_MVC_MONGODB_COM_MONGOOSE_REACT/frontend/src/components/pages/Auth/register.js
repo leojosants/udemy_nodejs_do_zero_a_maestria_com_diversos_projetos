de1b2a72b7fc8529919884fabbd4aa5529ecd11b
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-14 04:51:04 -0300

    Criando página e form de registro - RESOLVER NAV

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/components/pages/Auth/register.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/components/pages/Auth/register.js
@@ -1,11 +1,24 @@
 // 
-function Register() { 
+import Input from "../../form/input";
+
+function Register() {
+    function handleOnchange(e) { }
+
     return (
-
         <section>
-            <h1>Register</h1>
-        </section>
 
+            <h1>Registrar</h1>
+           
+            <form>
+                <Input
+                    name='name'
+                    id='name'
+                    placeholder='Digite o seu nome'
+                    onChange={handleOnchange}
+                />
+            </form>
+
+        </section>
     );
 };
 export default Register;
