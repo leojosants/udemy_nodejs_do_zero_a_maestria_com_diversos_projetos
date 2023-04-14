554825f773cffd2b009da483a46884102abc0ae3
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-14 18:24:11 -0300

    Criando contexto de usuário.

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/App.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/App.js
@@ -11,22 +11,27 @@ import Navbar from './components/layout/Navbar/Navbar';
 import Footer from './components/layout/Footer/Footer';
 import Container from './components/layout/Container/Container';
 
+//  Context
+import { UserProvider } from './context/UserContext';
+
 // 
 function App() {
   return (
 
     <Router>
-      <Navbar />
+      <UserProvider>
+        <Navbar />
 
-      <Container>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Container>
+        <Container>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
 
-      <Footer />
+        <Footer />
+      </UserProvider>
     </Router>
 
   );
