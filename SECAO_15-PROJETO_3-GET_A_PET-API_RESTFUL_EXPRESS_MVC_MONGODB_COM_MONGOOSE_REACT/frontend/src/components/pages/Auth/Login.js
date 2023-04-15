e7d4acda14d07f2b4afae8288b029f2351929f47
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-15 02:40:12 -0300

    Criando formulário de login.

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/components/pages/Auth/Login.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/components/pages/Auth/Login.js
@@ -1,11 +1,44 @@
 // 
-import React from 'react'
+import { useState, useContext } from 'react'
+import { Link } from 'react-router-dom';
+import Input from '../../../components/form/Input/Input';
+import styles from '../../form/Form.module.css';
 
+//  Context
+import { Context } from '../../../context/UserContext';
+
+// 
 function Login() {
+    function handleChange(e) { };
+
     return (
-        <section>
+
+        <section className={styles.form_container}>
             <h1>Login</h1>
+
+            <form>
+                <Input
+                    text='E-mail'
+                    type='email'
+                    name='email'
+                    placeholder='Digite o seu e-mail'
+                    handleOnChange={handleChange}
+                />
+
+                <Input
+                    text='Senha'
+                    type='password'
+                    name='password'
+                    placeholder='Digite a sua senha'
+                    handleOnChange={handleChange}
+                />
+
+                <input type="submit" value="Entrar" />
+            </form>
+
+            <p>Não tem conta? <Link to='/register'>Clique aqui!</Link></p>
         </section>
+
     );
 };
 
