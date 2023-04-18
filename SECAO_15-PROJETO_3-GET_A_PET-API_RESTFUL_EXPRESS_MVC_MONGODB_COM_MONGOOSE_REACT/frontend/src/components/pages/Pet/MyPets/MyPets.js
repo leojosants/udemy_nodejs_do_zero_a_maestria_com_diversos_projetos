99c3c0d742b312a8d8f3ebe9427e77c210c700de
Leonardo Santos <leonardojosantos@gmail.com>
2023-04-18 01:35:27 -0300

    Exibindo os pets na dashboard: erro imagem

--- SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/components/pages/Pet/MyPets/MyPets.js
+++ SECAO_15-PROJETO_3-GET_A_PET-API_RESTFUL_EXPRESS_MVC_MONGODB_COM_MONGOOSE_REACT/frontend/src/components/pages/Pet/MyPets/MyPets.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import RoundedImage from '../../../layout/RoundedImage/RoundedImage';
 import useFlashMessage from '../../../../hooks/useFlashMessage';
 import api from '../../../../utils/api';
+import styles from '../../Pet/Dashboard.module.css';
 
 // 
 function MyPets() {
@@ -32,11 +33,26 @@ function MyPets() {
                     pets.map((pet) => (
                         <div key={pet._id}>
                             <RoundedImage
-                                src={`${process.env.REACT_APP_API}/imagens/pets${pet.images[0]}`}
+                                src={`${process.env.REACT_APP_API}/imagens/pets/${pet.images[0]}`}
                                 alt={pet.name}
                                 width='75px'
                             />
-                            <span className="bold">{pet.name}</span>
+                            <span className="bold"> {pet.name}</span>
+                            <div className={styles.action}>
+                                {
+                                    pet.available ? (
+                                        <>
+                                            {
+                                                pet.adopter && (<button>Concluir adoção</button>)
+                                            }
+                                            <Link to={`/pet/edit/${pet._id}` }>Editar</Link>
+                                            <button>Excluir</button>
+                                        </>
+                                    ) : (
+                                        <p>Pet já adotado!</p>
+                                    )
+                                }
+                            </div>
                         </div>
                     ))
                 }
@@ -44,7 +60,7 @@ function MyPets() {
                     pets.length === 0 && <p>Não há Pets cadastrados</p>
                 }
             </div>
-        </section>
+        </section >
     );
 };
 
